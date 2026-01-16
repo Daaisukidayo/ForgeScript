dbaf4b24b15d1ac3d7d6b39ac5de6426f89ae354
xNickyDev <xnickydev@gmail.com>
2026-01-16 18:44:03 +0100

    Fixed $setTimeout and $setInterval cause crash on error

--- dist/native/array/isArray.js
+++ dist/native/array/isArray.js
@@ -7,6 +7,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const structures_1 = require("../../structures");
 exports.default = new structures_1.NativeFunction({
     name: "$isArray",
+    version: "2.7.0",
     description: "Checks whether given array is valid",
     aliases: ["$isValidArray"],
     unwrap: true,
